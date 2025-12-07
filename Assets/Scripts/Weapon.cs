1ea2c51b383211da5ba7a979f13dc0237a7ba952
Ha Nguyen <hanth05092004@gmail.com>
2025-12-07 11:47:03 +0700

    Raycasting

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] private float fireRate = 0.1f; // 0.1s = 10 viên/s
+    private MyInputs inputs;
+    private bool isShooting;
+    private float nextFireTime = 0f;
+    private void Awake()
+    {
+        inputs = new MyInputs();
+    }
+    private void OnEnable()
+    {
+        inputs.Enable();
+        inputs.Player.Shoot.started += OnShootStarted;
+        inputs.Player.Shoot.canceled += OnShootCanceled;
+    }
+
+    private void OnDisable()
+    {
+        inputs.Player.Shoot.started -= OnShootStarted;
+        inputs.Player.Shoot.canceled -= OnShootCanceled;
+        inputs.Disable();
+    }
+    private void Update()
+    {
+        if (isShooting && Time.time >= nextFireTime)
+        {
+            Shoot();
+            nextFireTime = Time.time + fireRate;
+        }
+    }
+    private void OnShootStarted(InputAction.CallbackContext ctx)
+    {
+        isShooting = true;
+    }
+
+    private void OnShootCanceled(InputAction.CallbackContext ctx)
+    {
+        isShooting = false;
+    }
+    private void OnShoot(InputAction.CallbackContext ctx)
+    {
+        Shoot();
+    }
+    private void Shoot()
+    {
+        RaycastHit hit;
+        Physics.Raycast(FPCamera.transform.position,FPCamera.transform.forward,out hit,range);
+        Debug.Log("I think this thing: "+ hit.transform.name);
+    }
+}
