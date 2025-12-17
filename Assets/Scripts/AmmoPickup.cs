d03f28c75056bf7b08113704032a4548d6addab4
Ha Nguyen <hanth05092004@gmail.com>
2025-12-18 00:19:21 +0700

    Ammo Pickup - Part 2

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -1,15 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour
 {
+    [SerializeField]
+    private int ammoAmount = 5;
+
+    [SerializeField]
+    private AmmoType ammoType;
+
+    private bool _isPickedUp = false;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
-        {
-            Debug.Log("Player did that players do");
-            Destroy(gameObject);
-        }
+        if (_isPickedUp)
+            return;
+
+        if (!other.TryGetComponent<Ammo>(out Ammo ammo))
+            return;
+
+        _isPickedUp = true;
+        ammo.IncreaseCurrentAmmo(ammoType, ammoAmount);
+        Destroy(gameObject);
     }
 }
